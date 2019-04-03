badcc55e36afc63b5f567f64a22789c1f23a1f92
kongty <kongty@chint.com>
2019-04-03 20:03:11 +0800

    add v3

---
+++ 行数统计/v3/do.js
@@ -0,0 +1,80 @@
+let allFun = {
+	filterFolder: ['node_modules', '.git'], // 需要过滤的文件夹
+	filterFile: ['.gitkeep'], // 需要过滤的文件,注意要加类型后缀
+	filterType: ['.png', '.jpg'], // 需要过滤的文件类型
+	onlyTypes: [], // 如果此数组有值，则只检查此数组内规定的文件类型
+	lineSum: 0, //总计代码行数
+	fileSum: -1, //总计文件数
+	timer: null,
+	init: function() {
+		let that = this;
+		document.querySelector('#btn').addEventListener('change', e => {
+			that.fileSum = e.target.files.length;
+			that.setTimer();
+			for (let entry of e.target.files) {
+				// console.log(entry.name, entry.webkitRelativePath, entry);
+				that.dispose(entry);
+			}
+		});
+	},
+	setTimer: function() {
+		let that = this;
+		if (that.timer) {
+			clearInterval(that.timer);
+		}
+		that.timer = setInterval(() => {
+			if (that.fileSum == 0) {
+				clearInterval(that.timer);
+				let output = '代码行数总计：' + allFun.lineSum;
+				console.log(output);
+				document.getElementById('output').innerText = output;
+			}
+		}, 2500);
+	},
+	dispose: function(file) {
+		let that = this;
+		let path = file.webkitRelativePath;
+		for (let i = 0, len = that.filterFolder.length; i < len; i++) {
+			let reg = new RegExp(that.filterFolder[i] + '/');
+			if (reg.test(path)) {
+				that.fileSum--;
+				return;
+			}
+		}
+        let name = file.name;        
+		if (that.filterFile.indexOf(name) != -1) {
+            that.fileSum--;
+		    return;
+		}
+		// let ext = path.extname(file);
+		// if (that.filterType.indexOf(ext) != -1) {
+        //     that.fileSum--;
+		//     return;
+		// }
+		// if (that.onlyTypes.length > 0 && that.onlyTypes.indexOf(ext) == -1) {
+        //     that.fileSum--;
+		//     return;
+		// }
+		that.doCount(file);
+	},
+	doCount: function(file) {
+		let that = this;
+		let reader = new FileReader();
+		reader.onload = function(e) {
+			that.lineGet(file.webkitRelativePath, e.target.result);
+		};
+		reader.readAsText(file); //发起异步请求
+	},
+	lineGet: function(fileName, fileText) {
+		let that = this;
+		let rep = fileText.toString();
+		let lines = rep.split('\n');
+		console.log(fileName + '    | ' + lines.length + ' |');
+		that.fileSum--;
+		that.lineSum += lines.length;
+	}
+};
+
+window.onload = function() {
+	allFun.init();
+};
